3a6db2f500e0d7dd67b5260e1fb40a4855e6a366
Bertan Karacora <s6bekara@uni-bonn.de>
2024-04-26 17:30:40 +0200

    Setup package

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+NAME_PACKAGE = "camera_ids"
+
+setuptools.setup(
+    name=NAME_PACKAGE,
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+    data_files=[
+        ("share/ament_index/resource_index/packages", [f"resource/{NAME_PACKAGE}"]),
+        (f"share/{NAME_PACKAGE}", ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Bertan Karacora",
+    maintainer_email="bertan.karacora@gmail.com",
+    description="ROS2 package for IDS camera",
+    license="MIT",
+    entry_points={
+        "console_scripts": [
+            "publish = camera_ids.publish_camera_ids:main",
+        ],
+    },
+)
