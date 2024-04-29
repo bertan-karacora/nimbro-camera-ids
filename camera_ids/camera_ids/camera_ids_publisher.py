ec332d8b7b501e49d28a8e2c521c7bb9d04ae8dc
Bertan Karacora <bertan.karacora@gmail.com>
2024-04-29 17:43:13 +0200

    Fix publishing

--- camera_ids/camera_ids/camera_ids_publisher.py
+++ camera_ids/camera_ids/camera_ids_publisher.py
@@ -1,36 +1,46 @@
-import cv2
+import importlib
+import importlib.resources
+
+from cv_bridge import CvBridge
 import ids_peak.ids_peak as idsp
 from rclpy.node import Node
-from sensor_msgs.msg import Image
 
 from camera_ids.camera_ids import CameraIDS
 
 
 class CameraIDSPublisher(Node):
-    def __init__(self):
+    def __init__(self, config_camera="default"):
         super().__init__("camera_ids_publisher")
-
+        self.bridge = None
         self.camera_ids = None
+        self.config_camera = None
         self.counter = None
         self.device = None
         self.device_manager = None
         self.publisher = None
         self.timer = None
 
-        self._initialize()
+        self._initialize(config_camera)
 
-    def _initialize(self):
+    def _initialize(self, config_camera):
         idsp.Library.Initialize()
 
         self.device_manager = idsp.DeviceManager.Instance()
         self.device = self.open_device()
         self.camera = CameraIDS(self.device)
+
+        self.config_camera = config_camera
+        path_config = importlib.resources.files(__package__) / "configs" / f"{self.config_camera}.cset"
+        self.camera.load_settings(path_config)
+
         self.camera.start_acquisition()
         self.camera.start_capturing()
 
-        self.counter = 0
-        self.timer = self.create_timer(1 / 30, self.timer_callback)
-        self.publisher = self.create_publisher(Image, "camera_ids", 10)
+        # self.bridge = CvBridge()
+        # self.counter = 0
+        # fps = 30
+        # self.timer = self.create_timer(1.0 / fps, self.timer_callback)
+        # self.publisher = self.create_publisher(Image, "camera_ids", 10)
 
     def print_device_info(device_descriptor):
         name_model = device_descriptor.ModelName()
@@ -43,28 +53,22 @@ class CameraIDSPublisher(Node):
         print(name_system)
         print(version_system)
 
-    def open_device(self):
+    def open_device(self, id_device=0):
         self.device_manager.Update()
         device_descriptors = self.device_manager.Devices()
 
-        if device_descriptors.empty():
-            print("No device found. Exiting Program.")
+        if device_descriptors.empty() or len(device_descriptors) <= id_device:
+            print("No device found.")
             return
 
-        device = device_descriptors[0].OpenDevice(idsp.DeviceAccessType_Control)
+        device = device_descriptors[id_device].OpenDevice(idsp.DeviceAccessType_Control)
         return device
 
     def timer_callback(self):
-        img = self.camera.capture()
+        img_ipl = self.camera.capture()
 
-        msg = Image()
-        msg.header.stamp = self.get_clock().now().to_msg()
-        # msg.header.frame_id = ""
-        # msg.height = np.shape(frame)[0]
-        # msg.width = np.shape(frame)[1]
-        # msg.encoding = "bgr8"
-        # msg.is_bigendian = False
-        msg.data = img.get_numpy_3D().tobytes()
+        img = img_ipl.get_numpy_3D()
+        msg = self.bridge.cv2_to_imgmsg(img, "bgra8")
 
         self.publisher.publish(msg)
         self.counter += 1
