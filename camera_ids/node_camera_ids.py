c66573005fbd314b9434ef0cd4ea68b7bf435fb7
Bertan Karacora <bertan.karacora@gmail.com>
2024-05-03 17:03:19 +0200

    Add template for node parameter implementation

--- camera_ids/node_camera_ids.py
+++ camera_ids/node_camera_ids.py
@@ -5,6 +5,8 @@ import sys
 from cv_bridge import CvBridge
 import ids_peak.ids_peak as idsp
 from rclpy.node import Node
+
+# from rcl_interfaces.msg import ParameterDescriptor, ParameterType, IntegerRange, FloatingPointRange, SetParametersResult
 from sensor_msgs.msg import Image
 
 from camera_ids.camera_ids import CameraIDS
@@ -35,31 +37,49 @@ class NodeCameraIDS(Node):
         path_config = importlib.resources.files(__package__) / "configs" / f"{self.config_camera}.cset"
         self.camera.load_settings(path_config)
 
-        self.camera.start_acquisition()
+        self.camera.start_capturing()
 
         self.bridge = CvBridge()
         self.counter = 0
+
+        # TODO: Where control fps aquisition and/or fps publishing
         fps = 30
         self.timer = self.create_timer(1.0 / fps, self.timer_callback)
         self.publisher = self.create_publisher(Image, "camera_ids", 10)
 
-    def print_device_info(device_descriptor):
+        # self.add_on_set_parameters_callback(self.parameter_callback)
+
+        # TODO: Add ROS Params. Config and fps?
+        # descriptor = ParameterDescriptor()
+        # descriptor.name = "my_param_name"
+        # descriptor.type = ParameterType.PARAMETER_INTEGER
+        # descriptor.description = "Size threshold of bounding box in pixels below which face detections are discarded"
+        # descriptor.read_only = False
+        # int_range = IntegerRange()
+        # int_range.from_value = 0
+        # int_range.to_value = 250000
+        # int_range.step = 1
+        # descriptor.integer_range.append(int_range)
+        # self.parameter_descriptors.append(descriptor)
+        # self.declare_parameter(descriptor.name, 30, descriptor)
+
+    def print_device_info(self, device_descriptor):
         name_model = device_descriptor.ModelName()
         name_interface = device_descriptor.ParentInterface().DisplayName()
         name_system = device_descriptor.ParentInterface().ParentSystem().DisplayName()
         version_system = device_descriptor.ParentInterface().ParentSystem().Version()
 
-        print(name_model)
-        print(name_interface)
-        print(name_system)
-        print(version_system)
+        self.get_logger().info(name_model)
+        self.get_logger().info(name_interface)
+        self.get_logger().info(name_system)
+        self.get_logger().info(version_system)
 
     def open_device(self, id_device=0):
         self.device_manager.Update()
         device_descriptors = self.device_manager.Devices()
 
         if device_descriptors.empty() or len(device_descriptors) <= id_device:
-            print("No device found.")
+            self.get_logger().info("No device found.")
             sys.exit()
 
         device = device_descriptors[id_device].OpenDevice(idsp.DeviceAccessType_Control)
@@ -74,3 +94,14 @@ class NodeCameraIDS(Node):
         self.publisher.publish(msg)
         self.counter += 1
         self.get_logger().info(f"Images published: {self.counter}")
+
+    # def parameter_callback(self, parameters):
+    #     result = SetParametersResult()
+    #     result.successful = True
+    #     result.reason = ""
+
+    #     for i in range(len(parameters)):
+    #         if parameters[i].name == "my_param_name":
+    #             self.my_param_name = parameters[i].value
+
+    #     return result
